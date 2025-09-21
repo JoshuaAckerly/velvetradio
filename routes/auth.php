c0f3130f00dfa54c9817a8ca511d82a436f628ac
Joshua Ackerly <ackerlyj05@gmail.com>
2025-09-21 15:05:19 -0400

    Changes to routes

--- routes/auth.php
+++ routes/auth.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+   /*  Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
@@ -31,11 +31,11 @@ Route::middleware('guest')->group(function () {
         ->name('password.reset');
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.store');
+        ->name('password.store'); */
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
+   /*  Route::get('verify-email', EmailVerificationPromptController::class)
         ->name('verification.notice');
 
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
@@ -53,5 +53,5 @@ Route::middleware('auth')->group(function () {
         ->middleware('throttle:6,1');
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-        ->name('logout');
+        ->name('logout'); */
 });
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
+Route::redirect('/login', '/', 301);
+Route::redirect('/register', '/', 301);
+Route::redirect('/forgot-password', '/', 301);
+Route::redirect('/reset-password', '/', 301);
+Route::redirect('/reset-password/*', '/', 301);
+Route::redirect('/email-verification', '/', 301);
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
