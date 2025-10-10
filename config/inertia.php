81bb1a1b32e965ac91114d6c6699d2f17936aa30
Joshua Ackerly <ackerlyj05@gmail.com>
2025-10-10 07:34:05 -0400

    SSR Port

--- config/inertia.php
+++ config/inertia.php
@@ -17,8 +17,8 @@ return [
 
     'ssr' => [
         'enabled' => true,
-        'url' => 'http://127.0.0.1:13714',
-        // 'bundle' => base_path('bootstrap/ssr/ssr.mjs'),
+        'url' => 'http://127.0.0.1:' . env('INERTIA_SSR_PORT', 13714),
+        'bundle' => base_path('bootstrap/ssr/ssr.js'),
 
     ],
 
