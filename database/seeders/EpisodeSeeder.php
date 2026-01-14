ec22350a4616afb0c2886cfe384e42f1563db2db
joshua <ackerlyj05@gmail.com>
2026-01-14 00:07:18 -0500

    Fix seeders: use dynamic show IDs instead of hardcoded values

--- database/seeders/EpisodeSeeder.php
+++ database/seeders/EpisodeSeeder.php
@@ -9,48 +9,50 @@ class EpisodeSeeder extends Seeder
 {
     public function run(): void
     {
+        $shows = \App\Models\Show::all();
+
         $episodes = [
             [
                 'title' => 'Lorem Ipsum Dolor',
                 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
                 'duration' => 2732, // 45:32
                 'published_at' => '2024-01-15',
-                'show_id' => 1,
+                'show_id' => $shows->first()->id,
             ],
             [
                 'title' => 'Consectetur Adipiscing',
                 'description' => 'Ut enim ad minim veniam, quis nostrud exercitation.',
                 'duration' => 2301, // 38:21
                 'published_at' => '2024-01-12',
-                'show_id' => 2,
+                'show_id' => $shows->skip(1)->first()->id,
             ],
             [
                 'title' => 'Sed Do Eiusmod',
                 'description' => 'Duis aute irure dolor in reprehenderit in voluptate.',
                 'duration' => 3138, // 52:18
                 'published_at' => '2024-01-10',
-                'show_id' => 3,
+                'show_id' => $shows->skip(2)->first()->id,
             ],
             [
                 'title' => 'Tempor Incididunt',
                 'description' => 'Excepteur sint occaecat cupidatat non proident.',
                 'duration' => 2505, // 41:45
                 'published_at' => '2024-01-08',
-                'show_id' => 1,
+                'show_id' => $shows->first()->id,
             ],
             [
                 'title' => 'Ut Labore Dolore',
                 'description' => 'Sunt in culpa qui officia deserunt mollit anim.',
                 'duration' => 2172, // 36:12
                 'published_at' => '2024-01-05',
-                'show_id' => 2,
+                'show_id' => $shows->skip(1)->first()->id,
             ],
             [
                 'title' => 'Magna Aliqua',
                 'description' => 'Sed ut perspiciatis unde omnis iste natus error.',
                 'duration' => 2913, // 48:33
                 'published_at' => '2024-01-03',
-                'show_id' => 3,
+                'show_id' => $shows->skip(2)->first()->id,
             ],
         ];
 
--- database/seeders/HostSeeder.php
+++ database/seeders/HostSeeder.php
@@ -9,24 +9,26 @@ class HostSeeder extends Seeder
 {
     public function run(): void
     {
+        $shows = \App\Models\Show::all();
+
         $hosts = [
             [
                 'name' => 'Alex Morgan',
                 'bio' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
                 'avatar' => 'AM',
-                'show_id' => 1,
+                'show_id' => $shows->first()->id,
             ],
             [
                 'name' => 'Sam Rivera',
                 'bio' => 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
                 'avatar' => 'SR',
-                'show_id' => 2,
+                'show_id' => $shows->skip(1)->first()->id,
             ],
             [
                 'name' => 'Jordan Blake',
                 'bio' => 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
                 'avatar' => 'JB',
-                'show_id' => 3,
+                'show_id' => $shows->skip(2)->first()->id,
             ],
         ];
 
